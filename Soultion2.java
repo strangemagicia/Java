c69a3251e3d06790549d806394fecd79f0fced33
김민경 <100396334+strangemagicia@users.noreply.github.com>
2025-01-19 11:59:42 +0900

    Create Soultion2.java

---
+++ Soultion2.java
@@ -0,0 +1,9 @@
+//정수 a,b가 주어질 때 a=? b=? 식으로 출력
+import java.util.Scanner;
+public class void main(String args[]){
+  Scanner sc = new Scanner(System.in);
+  int a = sc.nextInt();
+  int b = sc.nextInt();
+
+  System.out.println("a="+a+"\nb="+b);
+}
